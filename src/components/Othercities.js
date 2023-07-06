d6cd3aa5f219731ae0709fadebe3067b2777d01c
rachaouldbabaali <rachaould@gmail.com>
2023-07-06 15:46:06 +0100

    Style other cities

--- src/components/Othercities.js
+++ src/components/Othercities.js
@@ -1,20 +1,30 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  Button, Card, Col, Container, Row,
+  Card, Col, Container, Row,
 } from 'react-bootstrap';
 import { fetchWeather, setCity } from '../redux/weatherSlice';
 import '../styles/OtherCities.css';
 
+import ae from '../data/maps/ae.png';
+import au from '../data/maps/au.png';
+import br from '../data/maps/br.png';
+import fr from '../data/maps/fr.png';
+import gb from '../data/maps/gb.png';
+import in1 from '../data/maps/in.png';
+import jp from '../data/maps/jp.png';
+import us from '../data/maps/us.png';
+
 const cities = [
-  { name: 'New York', countryCode: 'us' },
-  { name: 'London', countryCode: 'gb' },
-  { name: 'Paris', countryCode: 'fr' },
-  { name: 'Tokyo', countryCode: 'jp' },
-  { name: 'Sydney', countryCode: 'au' },
-  { name: 'Dubai', countryCode: 'ae' },
-  { name: 'Mumbai', countryCode: 'in' },
-  { name: 'Rio de Janeiro', countryCode: 'br' },
+  { name: 'New York', countryCode: 'us', map: us },
+  { name: 'London', countryCode: 'gb', map: gb },
+  { name: 'Paris', countryCode: 'fr', map: fr },
+  { name: 'Tokyo', countryCode: 'jp', map: jp },
+  { name: 'Dubai', countryCode: 'ae', map: ae },
+  { name: 'Sydney', countryCode: 'au', map: au },
+  { name: 'Sao Paulo', countryCode: 'br', map: br },
+  { name: 'New Delhi', countryCode: 'in', map: in1 },
 ];
 
 const Weather = () => {
@@ -35,24 +45,19 @@ const Weather = () => {
   };
 
   return (
-    <div>
+    <div className="other">
       <Container>
         {cities.map((city) => (
           <Card
             key={city.name}
             onClick={() => handleCityClick(city)}
             className="city-card"
-            style={{ backgroundImage: "url('../data/maps/ae.png')" }}
           >
             <Card.Body>
               <Card.Title>{city.name}</Card.Title>
-              <Card.Text>{}</Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => handleCityClick(city)}
-              >
-                Details here
-              </Button>
+              <div className="city-map">
+                {city.map && <img src={city.map} />}
+              </div>
             </Card.Body>
           </Card>
 
