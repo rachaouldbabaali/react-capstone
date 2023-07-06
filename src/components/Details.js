1957e65f722cb7c1842b4af7decb194142e7838a
rachaouldbabaali <rachaould@gmail.com>
2023-07-06 19:02:24 +0100

    Style details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -10,101 +10,113 @@ const Details = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <div className="details">
-          <h1>
-            Weather Details for
-            <br />
-            {details.weather.weather.name}
-          </h1>
-          <div className="icon">
-            <img src={`http://openweathermap.org/img/w/${details.weather.weather.weather[0].icon}.png`} alt="weather icon" />
-          </div>
-          <table className="details-table">
-            <tbody>
-              <tr>
-                <td>
-                  The weather Today is :
-                  {' '}
-                  {' '}
-                  <span>
-                    {details.weather.weather.weather[0].description}
-                  </span>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  The temperature is :
-                  {' '}
-                  {' '}
-                  {Math.round(details.weather.weather.main.temp - 273.15)}
-                  {' '}
-                  °C
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  The Minimum temperature is :
-                  {' '}
-                  {' '}
-                  {Math.round(details.weather.weather.main.temp_min - 273.15)}
-                  {' '}
-                  °C
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  The Maximum temperature is :
-                  {' '}
-                  {' '}
-                  {Math.round(details.weather.weather.main.temp_max - 273.15)}
-                  {' '}
-                  °C
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  Feels like:
-                  {' '}
-                  {' '}
-                  {Math.round(details.weather.weather.main.feels_like - 273.15)}
-                  {' '}
-                  °C
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  The Wind speed is :
-                  {' '}
-                  {' '}
-                  {details.weather.weather.wind.speed}
-                  {' '}
-                  m/s
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  The Humidity is :
-                  {' '}
-                  {' '}
-                  {details.weather.weather.main.humidity}
-                  {' '}
-                  %
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  The Pressure is :
-                  {' '}
-                  {' '}
-                  {details.weather.weather.main.pressure}
-                  {' '}
-                  hPa
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div
+        className="weatherDetails"
+        key={details.weather.weather.id}
+      >
+        { details.weather.weather.name === 'area' ? (
+          <>
+            <h3>
+              Location Not Supported Yet !
+            </h3>
+          </>
+        )
+          : (
+            <div className="details">
+              <h1>
+                Weather Details for
+                <br />
+                {details.weather.weather.name}
+              </h1>
+              <div className="icon">
+                <img src={`http://openweathermap.org/img/w/${details.weather.weather.weather[0].icon}.png`} alt="weather icon" />
+              </div>
+              <table className="details-table">
+                <tbody>
+                  <tr>
+                    <td>
+                      The weather Today is :
+                      {' '}
+                      {' '}
+                      <span>
+                        {details.weather.weather.weather[0].description}
+                      </span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      The temperature is :
+                      {' '}
+                      {' '}
+                      {Math.round(details.weather.weather.main.temp - 273.15)}
+                      {' '}
+                      °C
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      The Minimum temperature is :
+                      {' '}
+                      {' '}
+                      {Math.round(details.weather.weather.main.temp_min - 273.15)}
+                      {' '}
+                      °C
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      The Maximum temperature is :
+                      {' '}
+                      {' '}
+                      {Math.round(details.weather.weather.main.temp_max - 273.15)}
+                      {' '}
+                      °C
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Feels like:
+                      {' '}
+                      {' '}
+                      {Math.round(details.weather.weather.main.feels_like - 273.15)}
+                      {' '}
+                      °C
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      The Wind speed is :
+                      {' '}
+                      {' '}
+                      {details.weather.weather.wind.speed}
+                      {' '}
+                      m/s
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      The Humidity is :
+                      {' '}
+                      {' '}
+                      {details.weather.weather.main.humidity}
+                      {' '}
+                      %
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      The Pressure is :
+                      {' '}
+                      {' '}
+                      {details.weather.weather.main.pressure}
+                      {' '}
+                      hPa
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
       </div>
     </>
   );
